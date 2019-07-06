da6cf211e6ecc2e859ca314c996df08615a17ce3
VrEmir <vranacemir@gmail.com>
2019-07-06 16:30:49 +0200

    Add Books class

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 // Book Class: Represents a Book
+class Books {
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
 
 // UI Class: Handle UI Task
 
